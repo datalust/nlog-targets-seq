0a44c57b4cfedb91319e68e201a6b9c0830efd44
Rolf Kristensen <sweaty1@hotmail.com>
2018-05-26 01:04:13 +0200

    SeqTarget - Added MaxRecursionLimit property to allow property reflection

--- src/NLog.Targets.Seq/RenderingsLayout.cs
+++ src/NLog.Targets.Seq/RenderingsLayout.cs
@@ -1,4 +1,18 @@
-﻿using System.IO;
+﻿// Seq Target for NLog - Copyright 2014-2017 Datalust and contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.IO;
 using System.Text;
 using NLog.Config;
 using NLog.Layouts;
@@ -11,37 +25,69 @@ namespace NLog.Targets.Seq
         IJsonConverter _jsonConverter;
         IJsonConverter JsonConverter => _jsonConverter ?? (_jsonConverter = ConfigurationItemFactory.Default.JsonConverter);
 
+        protected override void RenderFormattedMessage(LogEventInfo logEvent, StringBuilder target)
+        {
+            StringBuilder output;
+            RenderLogEvent(logEvent, target, out output);
+            if (output == null)
+                target.Append("null");
+        }
+
         protected override string GetFormattedMessage(LogEventInfo logEvent)
         {
-            StringBuilder output = null;
-            var nextDelimiter = "";
-            var mtp = logEvent.MessageTemplateParameters;
+            StringBuilder output;
+            RenderLogEvent(logEvent, null, out output);
+            return output?.ToString() ?? "null";
+        }
 
-            for (var i = 0; i < mtp.Count; ++i)
+        private void RenderLogEvent(LogEventInfo logEvent, StringBuilder preallocated, out StringBuilder output)
+        {
+            int orgLength = preallocated?.Length ?? 0;
+            output = null;
+
+            try
             {
-                var parameter = mtp[i];
+                var nextDelimiter = "";
+                var mtp = logEvent.MessageTemplateParameters;
 
-                if (parameter.CaptureType == CaptureType.Normal && parameter.Format != null)
+                for (var i = 0; i < mtp.Count; ++i)
                 {
-                    output = output ?? new StringBuilder("[");
-                    output.Append(nextDelimiter);
-                    nextDelimiter = ",";
+                    var parameter = mtp[i];
 
-                    var space = new StringWriter();
-                    var formatString = "{0:" + parameter.Format + "}";
+                    if (parameter.CaptureType == CaptureType.Normal && parameter.Format != null)
+                    {
+                        if (logEvent.Properties != null && logEvent.Properties.TryGetValue(parameter.Name, out var value))
+                        {
+                            if (output == null)
+                            {
+                                output = preallocated ?? new StringBuilder();
+                                output.Append("[");
+                            }
 
-                    if (logEvent.Properties != null && logEvent.Properties.TryGetValue(parameter.Name, out var value))
-                        space.Write(formatString, value);
+                            var formatString = string.Concat("{0:", parameter.Format, "}");
+                            var space = new StringWriter();
+                            space.Write(formatString, value);
 
-                    JsonConverter.SerializeObject(space.ToString(), output);
+                            output.Append(nextDelimiter);
+                            nextDelimiter = ",";
+                            JsonConverter.SerializeObject(space.ToString(), output);
+                        }
+                    }
                 }
             }
-
-            if (output == null)
-                return "null";
-
-            output.Append("]");
-            return output.ToString();
+            catch
+            {
+                if (output != null && preallocated != null)
+                    preallocated.Length = orgLength;    // truncate/unwind faulty output
+                output = null;
+                throw;
+            }
+            finally
+            {
+                if (output != null)
+                    output.Append("]");
+            }
         }
+
     }
 }
